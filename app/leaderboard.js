9e5e86f23fe10db3dfd5526f74d486685c9a60dc
Sae Hyun Song <17700563+saehyuns@users.noreply.github.com>
2017-10-10 11:34:56 -1000

    Task 5: Events, Sessions

--- app/leaderboard.js
+++ app/leaderboard.js
@@ -1,1 +1,32 @@
-console.log("HELLO WORLD");
+PlayersList = new Mongo.Collection('players');
+if(Meteor.isClient){
+  // this code only runs on the client
+  Template.leaderboard.helpers({
+    player() {
+      return PlayersList.find({});
+    },
+    'selectedClass': function(){
+      // code goes here
+      var playerId = this._id;
+      var selectedPlayer = Session.get('selectedPlayer');
+      if(playerId == selectedPlayer) {
+        return "selected";
+      }
+    }
+  }),
+  Template.leaderboard.events({
+    'click .player': function(){
+      // code goes here
+      var playerId = this._id;
+      Session.set('selectedPlayer', playerId);
+    }
+  });
+}
+if(Meteor.isServer){
+  // this code only runs on the server
+
+}
+/*
+import { Mongo } from 'meteor/mongo';
+export const PlayersList = new Mongo.Collection('players');
+*/
